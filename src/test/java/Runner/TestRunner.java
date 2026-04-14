0ac9a8e8934c9000f6e5951dc1b2759e5c4fe4a4
matecolic2 <matecolic2@gmail.com>
2026-04-14 14:37:56 +0200

    Java downgraded

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -6,7 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources",
         glue = {"Steps", "hooks", "Context"},
-        plugin = {"pretty"}
+        plugin = {
+                "pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
+
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
